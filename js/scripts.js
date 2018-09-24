39716accbcac1ebd17c4b0f636f00bfc7e64c9ac
GeGe-K <gloriagivondo@gmail.com>
2018-09-24 16:04:51 +0300

    added script to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,23 @@
 //value="10" for 10 marks, value="0" for 0 marks assigned to the radio buttons
-//
-// // Business Logic
-// var choises = [];
-// var total = 0;
-// var tabulate = function(){
-//   for (i=1; i<=3; i++){
-//     var answer = parseInt($("input:radio[name=qsn "+ i +"]:checked").val());
-//      if(isNaN(answer)){
-//        alert("Please ensure you fill all questions");
-//      }
-//      else {
-//        answer.push(answer);
-//      }//
-// // Business Logic
-// var choises = [];
-// var total = 0;
-// var tabulate = function(){
-//   for (i=1; i<=3; i++){
-//     var answer = parseInt($("input:radio[name=qsn "+ i +"]:checked").val());
-//      if(isNaN(answer)){
-//        alert("Please ensure you fill all questions");
-//      }
-//      else {
-//        answer.push(answer);
-//      }
-
-// for (i = 1; i <= 3; i++) {
-// var answer = $("input:radio[name=qsn"+i+"]:checked").val();
-// userInputs.push(answer);
-// }
-
-//   alert(answer);
-//   alert(choises);
-// });
-// var total = 0;
-// var tabulate = function(){
-//   var i=1
-//   // userInputs.forEach(function(userInput){
-//    var answer = $("input:radio[name=qsn"+i+"]:checked").val();
-//    // userInputs.push(answer);
-//    i+=1;
-//    if (answer===10){
-//      total
-//    }
-//  });
-document.ready(getElementById(){
+// Business Logic
+$(document).ready(function(){
   $("form#quiz-board").submit(function(event){
     var qsn1 = $("input:radio[name = qsn1]:checked").val();
     var qsn2 = $("input:radio[name = qsn2]:checked").val();
     var qsn3 = $("input:radio[name = qsn3]:checked").val();
     var total = parseInt(qsn1)+parseInt(qsn2)+parseInt(qsn3);
-    $("#display").text("Your total is" + total);
+    $("#total").text("Your total is" + total);
+// Front-End
+    $("button").click(function() {
+      $("#total-showing").toggle();
+      $("#quiz-board-hidden").toggle();
+    });
+    $("form#quiz-board").hide();
+    $("#total").show();
 
-    
-
-
-
-
-
-
-
-  };
-};
+    event.preventDefault();
+  });
+  $("#reload").click(function() {
+    location.reload();
+  })
+})
