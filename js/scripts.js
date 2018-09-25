87db00ad5e6f2f1c40cc5e8a36b6be067e1e3113
GeGe-K <gloriagivondo@gmail.com>
2018-09-25 08:57:11 +0300

    edited the reload button

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 //value="10" for 10 marks, value="0" for 0 marks assigned to the radio buttons
 // Business Logic
 $(document).ready(function(){
+  $("#reload").hide();
   $("form#quiz-board").submit(function(event){
     var qsn1 = $("input:radio[name = qsn1]:checked").val();
     var qsn2 = $("input:radio[name = qsn2]:checked").val();
     var qsn3 = $("input:radio[name = qsn3]:checked").val();
     var total = parseInt(qsn1)+parseInt(qsn2)+parseInt(qsn3);
-    $("#total").text("Your total is" + total);
+    $("#total").text("Your total score is" + "  " + total);
 // Front-End
     $("button").click(function() {
       $("#total-showing").toggle();
@@ -14,10 +15,11 @@ $(document).ready(function(){
     });
     $("form#quiz-board").hide();
     $("#total").show();
+    $("#reload").show();
 
     event.preventDefault();
   });
   $("#reload").click(function() {
     location.reload();
-  })
-})
+  });
+});
